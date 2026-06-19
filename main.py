7b72ef7258c753326c21b0776ff271c83f671d31
Adnan-Shahriar-1190 <sb.adnan2020@gmail.com>
2026-06-20 03:23:24 +0600

    added next_class tool

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def load_routine()->dict:
     with open('resources/aust_routine.json','r')as f:
         routine = json.load(f)
         return routine
+    
+def get_class_start_time(time:str)->str:
+    start_time = time.strip('-')[0].strip()
+    return start_time
         
 routine = load_routine()
     
@@ -33,25 +37,21 @@ def get_anyday_classes(day:str) ->str:
     day = day.upper().strip()
 
     if day not in routine['schedule']:
-        return f"Invalid day. No classes on {day}."
+        return {
+            "success":False,
+            "message":f"No classes for {day}."
+        }
     
     classes = routine['schedule'][day]
     
-    #print(classes)
-    
-    result = f"Classes on {day}: \n\n"
-    for cls in classes:
-        result += (
-            f"Course: {cls['course']} \n"
-            f"Time : {cls['time']} \n"
-            f"Room : {cls['room']} \n"
-            f"Teacher : {cls['teacher']} \n"
-            f"Type : {cls['type']} \n"
-        )
-    return result
+    return{
+        "success":True,
+        "day":day,
+        "classes": classes
+    }
 
 @mcp.tool
-def get_today_classes(day:str='',date:str='')->str:
+def get_today_all_classes()->str:
     """
     Get Today's class based on current day
     """
@@ -62,15 +62,46 @@ def get_today_classes(day:str='',date:str='')->str:
     
 
 @mcp.tool
-def get_next_class():
+def get_next_class_of_today():
     """
     Get the next class of the day based on current day and time.
     """
     current = get_current_datetime();
-    today = current["day"]
-    time = current['time']
+    current_day = current["day"]
+    current_time = current['time']
+    
+    if current_day not in routine['schedule']:
+        return {
+            "success":False,
+            "message":f"No classes for {current_day}."
+        }
+    
+    classes = routine['schedule'][current_day]
+    
+    next_class=None
+    for cls in classes:
+        start_time = get_class_start_time(cls['time'])
+        if start_time > current_time[:5]:
+            next_class = cls
+            break
+    
+    if next_class is None:
+        return {
+        "success": True,
+        "has_next_class": False,
+        "next_class": None,
+        "message": "No more classes Today"
+        
+        }
     
     
+    return {
+    "success": True,
+    "has_next_class": True,
+    "next_class": next_class,
+    "message": "Next class found."
+    }
+            
 
 @mcp.tool
 def get_course_info():
